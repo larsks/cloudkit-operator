b37ce6dfc08554d30dc0f3943ddf93d6f868303b
Lars Kellogg-Stedman <lars@redhat.com>
2025-03-27 09:52:15 -0400

    Teach controller how to create and manage resources
    When a new ClusterOrder is created, update .status.clusterReference and
    create dependent resources (namespace, service account, rolebinding). When
    a ClusterOrder is deleted, delete the namespace (and transitively delete
    all the resources contained in the namespace).
    
    Because we're creating resources in a namespace different from the one in
    which the ClusterOrder is created, we're relying on labels to manage watch
    events and trigger reconciliation.

---
+++ internal/controller/names.go
@@ -0,0 +1,9 @@
+package controller
+
+const (
+	defaultServiceAccountName string = "cloudkit"
+	defaultHostedClusterName  string = "cluster"
+	defaultRoleBindingName    string = "cloudkit"
+	cloudkitAppNameLabel      string = "cloudkit-operator"
+	cloudkitNamespaceName     string = "cloudkit-operator-system"
+)
